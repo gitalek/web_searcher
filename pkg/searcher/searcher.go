b0917b5979bfc8d476f13cf480b4e33808fb1c76
Alexander <shalek.edu@mail.ru>
2020-12-04 15:39:53 +1000

    add sequential searcher

---
+++ pkg/searcher/searcher.go
@@ -0,0 +1,35 @@
+package searcher
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+func Search(k string, urls []string) (map[string]int, error) {
+	results := make(map[string]int)
+
+	for _, url := range urls {
+		resp, err := http.Get(url)
+		if err != nil {
+			return results, err
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return results, err
+		}
+		
+		err = resp.Body.Close()
+		if err != nil {
+			return results, err
+		}
+
+		count := bytes.Count(body, []byte(k))
+		if _, ok := results[url]; !ok {
+			results[url] = count
+		}
+	}
+
+	return results, nil
+}
