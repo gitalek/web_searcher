20756c83ae38509e7ab04ba14be31ccd8ea2b4d2
Alexander <shalek.edu@mail.ru>
2020-12-09 23:49:49 +1000

    fixed tests: remove test with real request

--- pkg/searcher/searcher_test.go
+++ pkg/searcher/searcher_test.go
@@ -9,57 +9,6 @@ import (
 	"testing"
 )
 
-func TestSearchRealRequest(t *testing.T) {
-	urls := []string{
-		"http://example.com",
-		"https://opennet.ru",
-		"https://habr.com/ru/companies/",
-		"https://example.com",
-		"http://habr.com/ru/companies/",
-		"http://opennet.ru",
-		"https://go.dev/",
-		"http://go.dev/",
-		"http://go.dev/",
-		"http://go.dev/",
-	}
-	keyword := "background"
-	timeout := 2000
-	var limit int
-
-	// context
-	ctx := context.Background()
-
-	want := map[string]int{
-		"http://example.com":             2,
-		"http://go.dev/":                 0,
-		"http://habr.com/ru/companies/":  1,
-		"http://opennet.ru":              30,
-		"https://example.com":            2,
-		"https://go.dev/":                0,
-		"https://habr.com/ru/companies/": 1,
-		"https://opennet.ru":             30,
-	}
-
-	got := Search(ctx, keyword, urls, limit, timeout)
-
-	if len(got) != len(want) {
-		t.Fatalf("Wrong object received:\ngot:\t%v\nwant:\t%v", got, want)
-	}
-	for site, wantNum := range want {
-		gotNum, ok := got[site]
-		if !ok {
-			t.Errorf("Got object has no \"%s\" site\n", site)
-			t.Errorf("Wrong object received:\ngot:\t%#v\nwant:\t%#v", got, want)
-			break
-		}
-		if gotNum != wantNum {
-			t.Errorf("Objects don't match at site %s: got -> %d, want -> %d\n", site, gotNum, wantNum)
-			t.Errorf("Wrong object received:\ngot:\t%v\nwant:\t%v\n", got, want)
-			break
-		}
-	}
-}
-
 var sites = []string{
 	"http_example_com.html",
 	"http_go_dev.html",
